b773b0a11781adddc3f9ffa243bc8a4d1f7d1f20
Ardit Krasniqi <ardit.krasniqi@bexeo.org>
2018-12-05 14:14:11 +0100

    iframe fixes

--- themes/ardit/js/listeners.js
+++ themes/ardit/js/listeners.js
@@ -178,9 +178,7 @@ var Listeners = function () {
                     timeFormat: 'HH:mm'
                 });
             });
-            console.log(this.dom.frameModule);
-            if(this.dom.frameModule > 0){
-                alert('set localstorage item');
+            if(this.dom.frameModule.length > 0){
                 localStorage.setItem('frm', JSON.stringify(_this._objectifyForm(this.dom.frameModule.find('form'))));
             }
         }
